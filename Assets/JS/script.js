550c6cd721430efb11ee8af2f949511cdfaac011
DarianTHunter <DarianTHunter@outlook.com>
2020-07-17 23:02:59 -0400

    Additions

---
+++ Assets/JS/script.js
@@ -0,0 +1,15 @@
+$(".changeBtn1").click(function(){
+    console.log("click is working")
+});
+
+  $(".changeBtn2").click(function(){
+    console.log("click is working")
+});
+
+$(".changeBtn3").click(function(){
+    console.log("click is working")
+});
+
+$(".changeBtn1").click(function(){
+    console.log("click is working")
+});
